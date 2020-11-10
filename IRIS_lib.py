52a21e3e6d154479d4802a69e8fda12c2688a5cf
Momchil Molnar <momchil.molnar@colorado.edu>
2020-11-09 19:27:47 -0700

    initial commit

---
+++ IRIS_lib.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 18:39:21 2020
+
+Functions to do a few things:
+    1) open IRIS data files and load the proper spectral/SJ image;
+    2) and calculate the power spectrum of the properties and plot them;
+    3) Plot the results from 1) and 2);
+
+@author: molnarad
+"""
+
